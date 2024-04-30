19f1b0f99f722d09631530c9f0f75fade3b32ee6
Maximiliano Martino <Maxii.martino@gmail.com>
2024-04-30 20:05:52 -0300

    Connection between airflow and S3

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
     - intall airflow
     - set up airflow for the first time so you get the config files created
     - Change airflow dag folder to `dags_folder = /home/${USER}/repos/tp-avanzada/airflow/dags` replace USER with your username
+
+# Airflow
+    - start up airflow:
+    Run in one terminal `airflow scheduler`
+    In another terminal run `airflow webserver --port 8080`
+    - Set up the connection: 
+        In the UI: Admin > Connections
+
+
--- airflow/dags/airflow-ejemplo.py
+++ airflow/dags/airflow-ejemplo.py
@@ -1,6 +1,6 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.hooks.S3_hook import S3Hook
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import pandas as pd
 from io import StringIO
 from datetime import datetime
@@ -18,15 +18,19 @@ dag = DAG(
     'read_s3_csv_to_pandas',
     default_args=default_args,
     description='A DAG to read CSV files from S3 to Pandas DataFrame',
-    schedule_interval='@daily',
+    schedule='@daily',
 )
 
 def read_csv_from_s3(**kwargs):
-    s3_conn_id = 'your_s3_connection_id'
-    bucket_name = 'your_bucket_name'
-    key = 'path_to_your_csv_file.csv'
+    s3_conn_id = 'aws_default'
+    bucket_name = 'tpudesa'
+    key = 'ads_views.csv'
     s3_hook = S3Hook(aws_conn_id=s3_conn_id)
+    print(f"Reading CSV file from S3 bucket: {bucket_name}, key: {key}")
+    s3_conn = s3_hook.get_conn()
+    print(f"Connection to S3 established: {s3_conn}")
     csv_object = s3_hook.get_key(key, bucket_name)
+    print(f"CSV object: {csv_object}")
     csv_content = csv_object.get()['Body'].read().decode('utf-8')
     df = pd.read_csv(StringIO(csv_content))
     print(df.head())
